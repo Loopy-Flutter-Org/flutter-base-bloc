c0ea32fb51e8bbf483ebdccb2a4772f198e1fcf3
suni <sun-918@naver.com>
2022-03-16 18:01:19 +0900

    feat: Default Theme 설정
    Default Color, TextTheme, Brigthness 설정
    
    #2

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,24 +8,27 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // default value setting
+    const defaultPrimaryColor = Colors.pinkAccent;
+    const defaultBrightness = Brightness.dark;
+    const defaultTextTheme = TextTheme(
+        // Headline1..6, Large, Medium, Small
+        headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+        // BodyText1, 2, Large, Medium, Small
+        bodyText1: TextStyle(fontSize: 18.0),
+    );
+    // default theme setting
+    final theme = ThemeData(
+      primaryColor: defaultPrimaryColor,
+      brightness: defaultBrightness,
+      textTheme: defaultTextTheme,
+    );
+
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+      debugShowCheckedModeBanner: false,
+      theme: theme,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
