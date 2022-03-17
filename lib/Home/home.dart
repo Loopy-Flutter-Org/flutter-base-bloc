9721f1e76050a43808c5486348a0e48611f9a176
suni <sun-918@naver.com>
2022-03-18 02:50:11 +0900

    feat: UI 접근을 위한 home 세팅

---
+++ lib/Home/home.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:loopy_base_bloc/Pages/list.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Bass-Bloc'),
+        centerTitle: true,
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const ListPage()),
+              );
+            },
+            child: const Text('List UI'),
+          )
+          // ###Suni. 여기에 ElevatedButton 추가해서 UI 제작 예정
+        ],
+      ),
+    );
+  }
+}
