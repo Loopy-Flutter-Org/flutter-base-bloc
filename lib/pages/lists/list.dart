5932c4dc10ec075d8087bdeb4d295a385f45e4d7
Suni <sun-918@naver.com>
2022-03-26 14:14:42 +0900

    feat : ListView UI

--- lib/pages/lists/list.dart
+++ lib/pages/lists/list.dart
@@ -1,11 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_list_demo/default_list_page.dart';
+import 'package:flutter_list_demo/builder_list_page.dart';
+import 'package:flutter_list_demo/separated_list_page.dart';
 
-class ListPage extends StatelessWidget {
-  const ListPage({Key? key}) : super(key: key);
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    print("build ListPage");
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter List Demo'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => DefaultListPage()),
+                );
+              },
+              child: const Text('Default ListView Page'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const BuilderListPage()),
+                );
+              },
+              child: const Text('Builder ListView Page'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SeparatedListPage()),
+                );
+              },
+              child: const Text('SeparatorBuilder ListView Page'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
